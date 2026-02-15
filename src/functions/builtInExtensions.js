24683dd1ecb4888980819df863fc28d65f50be56
picreatoragain <logancssavinkoff@gmail.com>
2026-02-14 22:30:46 -0700

    ai slop thing to test if its even possible

--- src/functions/builtInExtensions.js
+++ src/functions/builtInExtensions.js
@@ -3,38 +3,9 @@ export default [
     id: "tween",
     name: "Tween",
     xml: `<category name="Tween" colour="#32a2c0">
-        <block type="tween_sprite_property">
-          <value name="TO">
-            <shadow type="math_number">
-              <field name="NUM">100</field>
-            </shadow>
-          </value>
-          <value name="DURATION">
-            <shadow type="math_number">
-              <field name="NUM">3</field>
-            </shadow>
-          </value>
-        </block>
-        <block type="tween_block">
-          <value name="FROM">
-            <shadow type="math_number">
-              <field name="NUM">0</field>
-            </shadow>
-          </value>
-          <value name="TO">
-            <shadow type="math_number">
-              <field name="NUM">100</field>
-            </shadow>
-          </value>
-          <value name="DURATION">
-            <shadow type="math_number">
-              <field name="NUM">3</field>
-            </shadow>
-          </value>
-        </block>
-        <block type="tween_block_value"></block>
-      </category>`,
+    </category>`,
   },
+
   {
     id: "pen",
     name: "Pen",
@@ -49,11 +20,16 @@ export default [
           </value>
         </block>
         <block type="set_pen_size">
-          <value name="SIZE"><shadow type="math_number"><field name="NUM">1</field></shadow></value>
+          <value name="SIZE">
+            <shadow type="math_number">
+              <field name="NUM">1</field>
+            </shadow>
+          </value>
         </block>
         <block type="clear_pen"></block>
       </category>`,
   },
+
   {
     id: "sets",
     name: "Sets",
@@ -64,35 +40,68 @@ export default [
         <sep gap="50"></sep>
         <block type="sets_size"></block>
         <block type="sets_isEmpty"></block>
-        <block type="sets_has">
-          <value name="VALUE">
-            <shadow type="text">
-                <field name="TEXT"></field>
-            </shadow>
-          </value>
-        </block>
-        <block type="sets_add_return">
-          <value name="VALUE">
-            <shadow type="text">
-                <field name="TEXT"></field>
-            </shadow>
-          </value>
-        </block>
-        <block type="sets_delete_return">
-          <value name="VALUE">
-            <shadow type="text">
-                <field name="TEXT"></field>
-            </shadow>
-          </value>
-        </block>
+        <block type="sets_has"></block>
+        <block type="sets_add_return"></block>
+        <block type="sets_delete_return"></block>
         <block type="sets_convert"></block>
         <block type="sets_merge"></block>
         <sep gap="50"></sep>
-        <block type="sets_foreach">
-          <value name="ITEM">
-            <shadow type="sets_foreach_item"></shadow>
-          </value>
-        </block>
+        <block type="sets_foreach"></block>
       </category>`,
+  },
+
+  // ✅ Runtime Variables category added here
+  {
+    id: "runtimevars",
+    name: "Runtime Variables",
+    xml: `<category name="Runtime Variables" colour="#d77824">
+      
+      <block type="SetTempVar">
+        <value name="var">
+          <shadow type="text">
+            <field name="TEXT">default</field>
+          </shadow>
+        </value>
+        <value name="setto">
+          <shadow type="text">
+            <field name="TEXT">6</field>
+          </shadow>
+        </value>
+      </block>
+
+      <block type="ChangeTempVar">
+        <value name="var">
+          <shadow type="text">
+            <field name="TEXT">default</field>
+          </shadow>
+        </value>
+        <value name="setto">
+          <shadow type="text">
+            <field name="TEXT">7</field>
+          </shadow>
+        </value>
+      </block>
+
+      <block type="ViewVar">
+        <value name="var">
+          <shadow type="text">
+            <field name="TEXT">default</field>
+          </shadow>
+        </value>
+      </block>
+
+      <block type="DeleteVar">
+        <value name="var">
+          <shadow type="text">
+            <field name="TEXT">default</field>
+          </shadow>
+        </value>
+      </block>
+
+      <block type="DeleteAll"></block>
+
+      <block type="ListVar"></block>
+
+    </category>`,
   },
 ];
